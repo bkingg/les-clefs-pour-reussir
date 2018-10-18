34258d2c2a2a6500b359c397e0d6c59564d77904
Les Clef Pour Reussir <gaspard+les-clefs-pour-reussir@revovision.ca>
2018-10-18 15:16:42 +0000

    Deploy from Forestry.io

--- js/script.js
+++ js/script.js
@@ -353,30 +353,15 @@
       if (errors) {
         return false;
       }
-    
-      $.ajax({
-        url: 'https:' + '//formspree.io/' + 'info' + '@' + 'lesclefspourreussir' + '.' + 'com', 
-        method: $(form).attr('method'),
-        data: $(form).serialize(),
-        dataType: 'json',
-        success: function(data){
-          console.log('success', data);
-          $(form).find('.status__failure').slideUp();
-          $(form).find('.status__success').slideDown();
-          $(form)[0].reset();
 
-          // Conversion Inscription
-          if($(form).attr('id') == 'contact-programme'){
-            // Event snippet for Inscription conversion page
-            gtag('event', 'inscription', {'send_to': 'AW-811137886/s9miCOigsX8Q3vbjggM'});
-          }
-        },
-        error: function(data){
-          console.log('error', data);
-          $(form).find('.status__failure').slideDown();
-          $(form).find('.status__success').slideUp();
-        }
-      });
+      $(form).attr('action', 'https:' + '//formspree.io/' + 'info' + '@' + 'lesclefspourreussir' + '.' + 'com');
+      $(form)[0].submit();
+
+      // Conversion Inscription
+      if($(form).attr('id') == 'contact-programme'){
+        // Event snippet for Inscription conversion page
+        gtag('event', 'inscription', {'send_to': 'AW-811137886/s9miCOigsX8Q3vbjggM'});
+      } 
   });
 
   $(document).on('click', '.inscrivez-vous', function(e){
