119ae87919ae56844ae24b1eda0b52ded85fd2c1
Les Clef Pour Reussir <gaspard+les-clefs-pour-reussir@revovision.ca>
2019-03-21 05:20:42 +0000

    Deploy from Forestry.io

--- js/script.js
+++ js/script.js
@@ -77,8 +77,6 @@
     $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
     //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
 
-    $('.menu > ul').before('<a href=\"#\" class=\"menu-mobile\">Menu</a>');
-
     //Adds menu-mobile class (for mobile toggle menu) before the normal menu
     //Mobile menu is hidden if width is more then 959px, but normal menu is displayed
     //Normal menu is hidden if width is below 959px, and jquery adds mobile menu
@@ -107,7 +105,7 @@
 
     $('.menu-mobile').on('click',function (e) {
         e.preventDefault();
-        $('.menu > ul').toggleClass('show-on-mobile');
+        $('.main-menu__nav').toggleClass('show-on-mobile');
     });
     //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
 
